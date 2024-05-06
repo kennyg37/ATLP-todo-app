20517aba0aa4192c52b7a5c0b0501b1978b8a4fc
kennyg37 <k.ganza@alustudent.com>
2024-05-06 12:28:45 +0200

    adding backend connection to the form components

--- frontend/src/components/Signup/signup.tsx
+++ frontend/src/components/Signup/signup.tsx
@@ -2,10 +2,46 @@ import React from "react";
 import './signup.css';
 
 const Signup: React.FC = () => {
+  const sendSignup = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get('email') as string;
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirmPassword') as string;
+
+    const requestBody = {
+      email: email,
+      username: username,
+      password: password,
+      confirmPassword: confirmPassword
+    };
+
+    try {
+      const response = await fetch('https://atlp-todo-app.onrender.com/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log('Signup successful');
+        HTMLFormElement.prototype.reset.call(event.currentTarget);
+      } else {
+        console.log('Signup failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle fetch error
+    }
+  };
   return (
     <div className="signup-container">
       <h1>Sign up</h1>
-        <form id="signup-form">
+        <form id="signup-form" onSubmit={sendSignup}>
             <label htmlFor="email">Email</label>
             <input type="email" name="email" id="email" />
             <label htmlFor="username">Username</label>
--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -1,15 +1,53 @@
-// components/Login.tsx
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import './login.css';
-import Signup from '../Signup/signup';
 
 const Login: React.FC = () => {
+  const [token, setToken] = useState<string | null>(null);
+
+  const sendLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const username = formData.get('username') as string;
+    const password = formData.get('password') as string;
+
+    const requestBody = {
+      username: username,
+      password: password,
+    };
+
+    try {
+      const response = await fetch('https://atlp-todo-app.onrender.com/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        setToken(data.token);
+        console.log('Login successful');
+        HTMLFormElement.prototype.reset.call(event.currentTarget);
+      } else {
+        console.log('Login failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      // Handle fetch error
+    }
+  };
+
   return (
     <div className='login-container'>
+      <div className='preloader'>
+        <div className='loader'></div>
+      </div>
       <h1>Login</h1>
-      <form id='login-form'>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
+      <form id='login-form' onSubmit={sendLogin}>
+        <label htmlFor="username">Username</label>
+        <input type="text" name="username" id="username" />
         <label htmlFor="password">Password</label>
         <input type="password" name="password" id="password" />
         <button type="submit">Login</button>
